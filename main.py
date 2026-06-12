0717af70cb12966dbd2d5046a201544af2d4eac5
geiseryessitflorezjaimes-code <geiseryessitflorezjaimes@gmail.com>
2026-06-12 02:01:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI
+from typing import Dict
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "TradeUp Bot Online"}
+    return {
+        "status": "TradeUp Bot Online",
+        "version": "1.0"
+    }
 
 @app.post("/webhook")
-async def webhook(data: dict):
+async def webhook(data: Dict):
     print("ALERTA RECIBIDA:", data)
+
+    score = data.get("score", 0)
+
+    if score >= 80:
+        return {
+            "success": True,
+            "accepted": True,
+            "message": "TradeUp aceptó la señal",
+            "data": data
+        }
 
     return {
         "success": True,
-        "message": "TradeUp recibió la señal",
+        "accepted": False,
+        "message": "Score menor a 80",
         "data": data
     }
